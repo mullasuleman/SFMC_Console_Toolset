110f9f161d5597d838cb3ad454adc81e405fe9e8
mullasuleman <mullasuleman12@gmail.com>
2024-10-30 17:42:11 -0400

    Update getDEFieldList.js

--- getDEFieldList.js
+++ getDEFieldList.js
@@ -1,23 +1,24 @@
 
 var tf = null;
 
-var mtf = (t) => {
-	var d = new Blob([t], { type: 'text/csv' });
-	if (tf !== null) window.URL.revokeObjectURL(tf);
-	tf = window.URL.createObjectURL(d);
-	return tf;
-};
-
-var df = (s) => {
-	console.log("Field List: \n\n" + s);
-	var fn = "fieldList_" + document.querySelector("#op-head-name-static").value;
-	var l = document.createElement('a');
-	l.setAttribute('download', `${fn}.csv`);
-	l.href = mtf(s);
-	l.click();
-};
-
 var getDEFieldList = () => {
+
+	var mtf = (t) => {
+		var d = new Blob([t], { type: 'text/csv' });
+		if (tf !== null) window.URL.revokeObjectURL(tf);
+		tf = window.URL.createObjectURL(d);
+		return tf;
+	};
+
+	var df = (s) => {
+		console.log("Field List: \n\n" + s);
+		var fn = "fieldList_" + document.querySelector("#op-head-name-static").value;
+		var l = document.createElement('a');
+		l.setAttribute('download', `${fn}.csv`);
+		l.href = mtf(s);
+		l.click();
+	};
+
 	if (document.querySelectorAll(".de-content-fields-listings").length <= 0)
 		return "No Data Extension Found............................... Change iframe context to Email ⇓";
 	var fl = [];
